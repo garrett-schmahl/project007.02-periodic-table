5f59c733c461ee658b803284490c363d8c3b9bd3
Garrett Schmahl <gschmahl@pm.me>
2021-03-25 17:20:43 -0700

    initial formatting, divs

--- gitInit.bat
+++
@@ -1,6 +0,0 @@
-git init
-git add .
-git commit -m "initialize repository"
-git branch -M main
-git remote add origin https://github.com/garrett-schmahl/project007.02-periodic-table.git
-git push -u origin main
